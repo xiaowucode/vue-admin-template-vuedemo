f3db0762ab0bd1b57024fd3586038cf7301de309
xiaowucode <727555063@qq.com>
2021-08-07 22:45:50 +0800

    test

---
+++ src/components/MyItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <li>
+    <label>
+      <input type="checkbox" :checked="todo.done" @change="handleCheck(todo.id)"/>
+      <span>{{todo.title}}</span>
+    </label>
+    <button class="btn btn-danger">删除</button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'MyItem',
+  props: ['todo',"checkTodo"],
+  methods: {
+    handleCheck(id) {
+      this.checkTodo(id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+ li {
+   list-style: none;
+   height: 36px;
+   line-height: 36px;
+   padding: 0 5px;
+   border-bottom: 1px solid #ddd;
+ }
+ li label {
+   float: left;
+   cursor: pointer;
+ }
+ li label li input {
+   vertical-align: middle;
+   margin-right: 6px;
+   position: relative;
+   top: -1px;
+ }
+.btn-danger{
+   float: right;
+   display: none;
+   margin-top: 3px;
+ }
+ li:before {
+   content: initial;
+ }
+ li:last-child {
+   border-bottom: none;
+ }
+ li:hover {
+   background-color: #ddd;
+ }
+</style>
